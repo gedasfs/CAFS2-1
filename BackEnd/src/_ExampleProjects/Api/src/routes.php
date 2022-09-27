27b62b273c38e6f2f044219b37cb7d7c5df712e8
Kiril <hello@nonamez.name>
2022-09-27 19:29:08 +0300

    Added middleware example

--- BackEnd/src/_ExampleProjects/Api/src/routes.php
+++ BackEnd/src/_ExampleProjects/Api/src/routes.php
@@ -6,9 +6,9 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 $app->get('/', function(Request $request, Response $response) {
-    // $response->getBody()->write('Hello World');
+    $response->getBody()->write('Hello World');
 
-	$response->getBody()->write(file_get_contents(ROOT_PATH . '/views/index.html'));
+	// $response->getBody()->write(file_get_contents(ROOT_PATH . '/views/index.html'));
 
 	return $response;
 });
